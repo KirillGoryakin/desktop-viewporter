a5d3141578aa8d48b539251e0feefb9ecd9c812d
Kirill Goryakin <kirillgoryakin.dev@gmail.com>
2023-12-31 20:49:02 +0500

    Add icons

--- src/background.js
+++ src/background.js
@@ -9,3 +9,28 @@ chrome.action.onClicked.addListener(async (tab) => {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.tabs.sendMessage(tab.id, { type: "toggle" });
 });
+
+chrome.runtime.onMessage.addListener((request) => {
+  switch (request.colorScheme) {
+    case 'light': 
+      chrome.action.setIcon({
+        path: {
+          "16": "icons/icon_16.png",
+          "32": "icons/icon_32.png",
+          "48": "icons/icon_48.png",
+          "128": "icons/icon_128.png"
+        }
+      });
+      break;
+    case 'dark': 
+      chrome.action.setIcon({
+        path: {
+          "16": "icons/icon_16_white.png",
+          "32": "icons/icon_32_white.png",
+          "48": "icons/icon_48_white.png",
+          "128": "icons/icon_128_white.png"
+        }
+      });
+      break;
+  }
+});
--- src/contentScript.js
+++ src/contentScript.js
@@ -3,19 +3,23 @@
 let enabled = false;
 
 chrome.runtime.onMessage.addListener((request) => {
-  if (request.type === 'toggle') {
-    enabled = !enabled;
+  if (request.type !== 'toggle') return;
 
-    const viewport = document.querySelector('meta[name=viewport]');
-    if (!viewport) {
-      alert('meta viewport is not found!');
-      return;
-    }
-    
-    if (enabled) {
-      viewport.setAttribute('content', 'width=1440px, height=device-height, initial-scale=1.0');
-    } else {
-      viewport.setAttribute('content', 'width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0');
-    }
+  enabled = !enabled;
+
+  const viewport = document.querySelector('meta[name=viewport]');
+  if (!viewport) {
+    alert('meta viewport is not found!');
+    return;
   }
+  
+  if (enabled) {
+    viewport.setAttribute('content', 'width=1440px, height=device-height, initial-scale=1.0');
+  } else {
+    viewport.setAttribute('content', 'width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0');
+  }
+});
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+  chrome.runtime.sendMessage({ colorScheme: event.matches ? 'dark' : 'light' });
 });
